12cc539392d996d7b2c597c15d38c66c9a60d64b
Logan Demmy <frank.demmy@gmail.com>
2021-04-01 14:45:21 -0500

    first commit

---
+++ src/components/employees/EmployeeCard.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Emma Beaton</h3>
+        <div className="employee__location">Nashbville Kennels North</div>
+    </section>
+)
---
+++ src/components/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels North</h3>
+        <div className="location__address">200 Main St</div>
+    </section>
+)
