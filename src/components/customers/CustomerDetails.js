06f0e90075e7d60b0bac6fff42b0975af4f4731b
Logan Demmy <frank.demmy@gmail.com>
2021-04-07 09:46:03 -0500

    isLoading function to prevent multiple deletes

--- src/components/customers/CustomerDetails.js
+++ src/components/customers/CustomerDetails.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react"
-import { getCustomerById } from "../../modules/CustomerManager"
-import { useParams } from "react-router-dom"
+import { getCustomerById, removeCustomer } from "../../modules/CustomerManager"
+import { useHistory, useParams } from "react-router-dom"
 
 export const CustomerDetail = () => {
     const [customer, setCustomer] = useState({name: "", address:""})
+    const [isLoading, setIsLoading] = useState(true)
 
     const {customerId} =useParams();
+    const history = useHistory();
+
+    const handleDelete = () => {
+        setIsLoading(true);
+        removeCustomer(customerId).then(() =>
+            history.push("/customers")
+        )
+    }
 
     useEffect(() => {
         getCustomerById(customerId)
@@ -15,14 +24,20 @@ export const CustomerDetail = () => {
                 address: customer.address,
                 animal: customer.animal
             })
+            setIsLoading(false)
         })
     }, [customerId])
+
     return (
         <section className="customer">
           <h3 className="customer__name">{customer.name}</h3>
           <div className="customer__breed">{customer.address}</div>
           {/* What's up with the question mark???? See below.*/}
           <div className="customer__location">Animal: {customer.animal?.name}</div>
+        
+            <button type="button" disabled={isLoading} onClick={handleDelete}>
+                Remove
+            </button>
         </section>
       );
 }
