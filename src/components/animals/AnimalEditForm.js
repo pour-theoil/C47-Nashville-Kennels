5e8f2c8960f2cae87a8064a423323a19ab6a366d
Logan Demmy <frank.demmy@gmail.com>
2021-04-08 10:44:41 -0500

    added an edit form

---
+++ src/components/animals/AnimalEditForm.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react"
+import { useParams, useHistory} from "react-router-dom"
+import { updateAnimal, getAnimalById } from "../../modules/AnimalManager"
+import "./AnimalForm.css"
+
+
+export const AnimalEditForm = () => {
+  const [animal, setAnimal] = useState({ name: "", breed: "" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const {animalId} = useParams();
+  const history = useHistory();
+
+  const handleFieldChange = evt => {
+    const stateToChange = { ...animal };
+    stateToChange[evt.target.id] = evt.target.value;
+    setAnimal(stateToChange);
+  };
+
+  const updateExistingAnimal = evt => {
+    evt.preventDefault()
+    setIsLoading(true);
+
+    // This is an edit, so we need the id
+    const editedAnimal = {
+      id: animal.Id,
+      name: animal.name,
+      breed: animal.breed
+    };
+
+  updateAnimal(editedAnimal)
+    .then(() => history.push("/animals")
+    )
+  }
+
+  useEffect(() => {
+    getAnimalById(animalId)
+      .then(animal => {
+        setAnimal(animal);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <form>
+        <fieldset>
+          <div className="formgrid">
+            <input
+              type="text"
+              required
+              className="form-control"
+              onChange={handleFieldChange}
+              id="name"
+              value={animal.name}
+            />
+            <label htmlFor="name">Animal name</label>
+
+            <input
+              type="text"
+              required
+              className="form-control"
+              onChange={handleFieldChange}
+              id="breed"
+              value={animal.breed}
+            />
+            <label htmlFor="breed">Breed</label>
+          </div>
+          <div className="alignRight">
+            <button
+              type="button" disabled={isLoading}
+              onClick={updateExistingAnimal}
+              className="btn btn-primary"
+            >Submit</button>
+          </div>
+        </fieldset>
+      </form>
+    </>
+  );
+}
